805498a7b38fcfd4b2657e7567e3fcaa8e9a5f64
webiest <webiest@46f8bf6a-1752-0410-96e1-5992aa8baeaf>
2008-07-16 05:20:07 +0000

    adding route for logout link

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :session
   map.resources :bubbles
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -36,6 +37,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Install the default routes as the lowest priority. 
   map.root :controller => 'bubbles'
+  
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
