d393e0cb2a6e593ab9c6a8cce9a2467afba4f9dc
oristian <oristian@46f8bf6a-1752-0410-96e1-5992aa8baeaf>
2008-08-27 18:57:09 +0000

    updated paths for User edits

--- trunk/app/controllers/users_controller.rb
+++ trunk/app/controllers/users_controller.rb
@@ -23,5 +23,11 @@ class UsersController < ApplicationController
   def edit  
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to bubbles_path
+  end
 
 end
