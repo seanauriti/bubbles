e231b71065054acba0aec868fba9cd112ab2d092
kylebanker <kylebanker@46f8bf6a-1752-0410-96e1-5992aa8baeaf>
2008-08-27 20:50:47 +0000

    Removed forgery protection.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time   
   include AuthenticatedSystem
-  protect_from_forgery # :secret => 'd933d30c61064fdaa66a94e4220fa1a8'  
+  #protect_from_forgery # :secret => 'd933d30c61064fdaa66a94e4220fa1a8'  
   
   protected
     def update_last_retrieval_time
