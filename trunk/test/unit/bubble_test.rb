88701248a06e9e05aff0cc91fbca5def882709bb
oristian <oristian@46f8bf6a-1752-0410-96e1-5992aa8baeaf>
2008-09-02 16:21:41 +0000

    bubble unit test drastically shortened with shoulda

--- trunk/test/unit/bubble_test.rb
+++ trunk/test/unit/bubble_test.rb
@@ -1,32 +1,13 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class BubbleTest < ActiveSupport::TestCase
-  fixtures :bubbles, :users
+class BubbleTest < Test::Unit::TestCase 
 
-  def test_should_create_bubble
-    assert_difference 'Bubble.count' do
-      bubble = create_bubble 
-      assert !bubble.new_record?
+  context "A Bubble instance" do
+    setup do
+      b = Factory(:bubble)
     end
+    
+    should_ensure_length_at_least :body, 1  
   end
   
-  def test_should_not_allow_blank_bodies
-    bubble = create_bubble(:body => '')
-    assert bubble.new_record?
-    assert !bubble.valid?
-  end
-  
-  def test_should_destroy_bubble
-    assert_difference 'Bubble.count' do
-      bubble = bubbles(:two)
-      bubble.expire
-    end
-  end     
-  
-protected
-  def create_bubble(options = {})
-    record = Bubble.new({ :body => 'test bubble', :user => users(:quentin)}.merge(options))
-    record.save
-    record
-  end
 end
