76351c4c9d81303e5f0e36e4c7ab57b117a3c9ff
oristian <oristian@46f8bf6a-1752-0410-96e1-5992aa8baeaf>
2008-09-04 22:28:50 +0000

    Testing changes - shoulda had a V8

--- test/unit/bubble_test.rb
+++ test/unit/bubble_test.rb
@@ -2,12 +2,13 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class BubbleTest < Test::Unit::TestCase 
 
+  should_ensure_length_at_least :body, 1
+  should_belong_to              :user   
+
   context "A Bubble instance" do
     setup do
       b = Factory(:bubble)
     end
-    
-    should_ensure_length_at_least :body, 1  
   end
   
 end
