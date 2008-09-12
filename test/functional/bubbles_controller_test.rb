8d6fb71907f6ddc22e192b68adbc21e89d9381a9
oristian <oristian@46f8bf6a-1752-0410-96e1-5992aa8baeaf>
2008-09-12 17:36:19 +0000

    bubbles tests

--- test/functional/bubbles_controller_test.rb
+++ test/functional/bubbles_controller_test.rb
@@ -1,44 +1,58 @@
 require File.dirname(__FILE__) + '/../test_helper'
 class BubblesControllerTest < ActionController::TestCase
 
-  setup do
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new 
-  end
-  
-  context "GET :index" do
+  context "a bubbles controller " do
     setup do
-      @controller.stubs(:current_user).returns(Factory(:user))     
-      get :index
+      @request    = ActionController::TestRequest.new
+      @response   = ActionController::TestResponse.new
+      @controller.stubs(:current_user).returns(Factory(:user))   
     end
+  
+    context "GET :index" do
+      setup do
+        get :index
+      end
       
-    should_assign_to    :bubbles                                
-    should_respond_with :success
-  end 
-  
-  context "on Create" do
-    setup do
-      post :create, :bubble => Factory.attributes_for(:bubble)
-    end
-    
-    should_respond_with :redirect             
-  end    
-             
-  context "on Pop!" do
-    setup do
-      @controller.stubs(:current_user).returns(Factory(:user)) 
-      @bubble1 = Factory(:bubble)
-      @bubble2 = Factory(:another_bubble)
-
-      puts "Count => #{@count = Bubble.find_unsolved.count}" 
-      post :destroy, :id => @bubble1.id 
-    end
-    
-    should "have one less unsolved bubble" do 
-      assert_match /Effect\.Fade/, @response.body           
-      assert_equal @count - 1, Bubble.find_unsolved.count   
+      should_assign_to    :bubbles                                
+      should_respond_with :success
     end 
-  end
+  
+    context "on Create" do
+      setup do
+        post :create, :bubble => Factory.attributes_for(:bubble)
+      end
+    
+      should_respond_with :success
+         
+    end
+  
+    context "on Append" do
+      setup do
+        post :append, :body => 'try to append this'
+      end
+      
+      should_respond_with :success
+      should "get back an assertion" do
+        assert_match /Insertion/, @response.body
+      end    
+    end
+             
+    context "on Pop!" do
+      setup do
+
+        @bubble1 = Factory(:bubble)
+        @bubble2 = Factory(:another_bubble)
+
+        puts "Count => #{@count = Bubble.find_unsolved.count}" 
+        post :destroy, :id => @bubble1.id 
+      end
+    
+      should "have one less unsolved bubble" do 
+        assert_match /Effect\.Fade/, @response.body           
+        assert_equal @count - 1, Bubble.find_unsolved.count   
+      end 
+    end
+  end                
   
   # rewrite of the login_as(:user) from AuthenticatedTestHelper, which uses fixtures
   def login_as(user)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+$:.reject! { |e| e.include? 'TextMate' }
+ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
